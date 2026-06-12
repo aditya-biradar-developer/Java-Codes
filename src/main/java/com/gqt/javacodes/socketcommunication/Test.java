4c3c45c21a7f1e9d718f85b93081f6269f8cddb0
adityabiradar018 <helloadityabiradar013@gmail.com>
2026-06-12 09:00:04 +0530

    Test file uploaded

---
+++ src/main/java/com/gqt/javacodes/socketcommunication/Test.java
@@ -0,0 +1,10 @@
+package com.gqt.javacodes.socketcommunication;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+	}
+
+}
